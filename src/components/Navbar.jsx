bbc8d2abd43e9ba85c4cafffb0542c0c73b02c67
Abhishek Pal <abhishek1100861@gmail.com>
2022-03-31 07:30:07 +0530

    Desktop Navbar completed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className='flex justify-around basis-full items-center pt-4'>
+    <div className='flex basis-1/6 justify-end'>
+      <h2 className='text-xl font-semibold'>Shortly</h2>
+    </div>
+        <div className='flex basis-6/12  justify-items-start font-medium text-slate-400'>
+          <h3 className='mr-8'>Features</h3>
+          <h3 className='mr-8'>Pricing</h3>
+          <h3 className='mr-8'>Resources</h3>
+        </div>
+
+        <div className='flex basis-3/12 justify-items-center'>
+          <button className='text-slate-500 font-normal'>Login</button>
+          <button className='ml-8 w-20 font-semibold p-0.5 text-stone-100  bg-cyan-500 hover:bg-cyan-600 rounded-full'>Sign Up</button>
+        </div>
+    </div>
+  )
+}
